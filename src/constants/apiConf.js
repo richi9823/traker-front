7eedbc6ddcbf3a8b50e6d9a77e720f901dc07d68
Ricardo Galende <ricardo.galende@globalincubator.com>
2024-03-24 15:37:14 +0100

    address

--- src/constants/apiConf.js
+++ src/constants/apiConf.js
@@ -1,5 +1,5 @@
 export const TrakerApi = require('TrakerApi');
-export const API_BASE_URL = "http://200.234.235.51:8080";
+export const API_BASE_URL = "https://200.234.235.51:8080";
 const defaultClient = TrakerApi.ApiClient.instance;
 defaultClient.timeout = 500000;
 // Configure API URL
