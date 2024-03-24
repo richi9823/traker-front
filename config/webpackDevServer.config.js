01fb4c93c67ef7572a8a678b7c257e7c59501054
Ricardo Galende <ricardo.galende@globalincubator.com>
2024-03-24 20:06:47 +0100

    crt

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -11,8 +11,8 @@ const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || '0.0.0.0';
 
 module.exports = function(proxy, allowedHost) {
-  const cert = fs.readFileSync(path.join(__dirname, 'certificate.crt'), 'utf8');
-const key = fs.readFileSync(path.join(__dirname, 'privatekey.key'), 'utf8');
+  const cert = fs.readFileSync(path.join(__dirname, 'traker_front.crt'), 'utf8');
+const key = fs.readFileSync(path.join(__dirname, 'traker_front.key'), 'utf8');
   return {
     // WebpackDevServer 2.4.3 introduced a security fix that prevents remote
     // websites from potentially accessing local content through DNS rebinding:
